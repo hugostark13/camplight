394a86c4d8e4fa22da92cd52b342f7f77332af72
Hugo Stark <hugostark@Hugos-MacBook-Pro.local>
2024-05-23 14:21:18 +0300

    Add form validation.

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, Regexp
+import email_validator
 
 
 class Search(FlaskForm):
@@ -9,9 +10,26 @@ class Search(FlaskForm):
 
 
 class Add(FlaskForm):
-    name = StringField("Name", validators=[DataRequired("Please enter your name.")])
-    email = StringField(
-        "E-Mail", validators=[DataRequired("Please enter your email address.")]
+    name = StringField(
+        "Name",
+        validators=[DataRequired("Please enter your name."), Length(min=2, max=50)],
     )
-    phone = StringField("Phone", validators=[DataRequired("Please enter your phone.")])
+    email = StringField(
+        "E-Mail",
+        validators=[
+            DataRequired("Please enter your email address."),
+            Email(message="Invalid email address."),
+            Length(max=120),
+        ],
+    )
+    phone = StringField(
+        "Phone",
+        validators=[
+            DataRequired("Please enter your phone."),
+            Regexp(
+                r"^\+?\d{8,15}$",
+                message="Invalid phone number format. (e.g., +359889789)",
+            ),
+        ],
+    )
     submit = SubmitField("Add")
